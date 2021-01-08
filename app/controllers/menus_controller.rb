b5ffee687e2edd744219283d4a3dc575bce90610
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-01-08 21:04:48 +0900

    献立表

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,19 @@
 class MenusController < ApplicationController
+   def index
+      @menus = Menu.all
+   end
+
+   def new
+      @menu = Menu.new
+   end
+
+   def create
+      Menu.create(menu_params)
+      redirect_to action: :index
+   end
+
+   private
+   def menu_params
+      params.require(:menu).permit(:name,:sun_menu,:sun_making,:mon_menu,:mon_making,:tue_menu,:tue_making)
+   end
 end
