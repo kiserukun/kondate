8c5bf7c6f5ef46bc2d04ccdc5aee676dd5374800
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-01-10 15:26:00 +0900

    css編集

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -12,6 +12,10 @@ class MenusController < ApplicationController
       redirect_to action: :index
    end
 
+   def show
+      @menu = Menu.find(params[:id])
+    end
+
    private
    def menu_params
       params.require(:menu).permit(:name,:sun_menu,:sun_making,:mon_menu,:mon_making,:tue_menu,:tue_making,:wed_menu,:wed_making,:thu_menu,:thu_making,:fri_menu,:fri_making,:sat_menu,:sat_making)
